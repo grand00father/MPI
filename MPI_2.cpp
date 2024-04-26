5a969c63d9abb81419ae5f3a24405ea75cdb2b66
grand00father <121361298+grand00father@users.noreply.github.com>
2024-04-26 17:33:58 +0300

    Update MPI_2.cpp

--- MPI_2.cpp
+++ MPI_2.cpp
@@ -5,67 +5,79 @@
 
 int main(int argc, char** argv)
 {
-	const int MAX = 1000;
-	int a[MAX];
-	
+	const int MAX_1 = 10;
+	const int MAX_2 = 1000;
+	const int MAX_3 = 1000000;
+	int max_sizes[] = { MAX_1, MAX_2, MAX_3 };
+
 	int rank, size, processor_name_length;
 	int sum;
-	int* b= &sum;
+	int* b = &sum;
 	int n, ibeg, iend;
 	char* processor_name = new char[MPI_MAX_PROCESSOR_NAME * sizeof(char)];
-
-	for (int i = 0; i < MAX; i++) {
-		a[i] = i+1;
-	}
-
-	auto start = std::chrono::system_clock::now();
-
-	MPI_Status status;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	n = (int) MAX / size;
-	ibeg = rank * n;
-	iend = (rank + 1) * n;
+	MPI_Status status;
 
-	
-	if (rank == 0){
+	double total_elapsed_time = 0.0;
 
-		sum = 0;
-		
-		for (int i = ibeg; i < ((iend > MAX) ? MAX : iend); i++)
-		{
-			sum += a[i];
+	for (int max_size : max_sizes) {
+		int* a = new int[max_size];
+
+		for (int i = 0; i < max_size; i++) {
+			a[i] = i + 1;
 		}
-		printf("Process: %d, %d\n", rank, sum);
-		MPI_Send(b,1, MPI_INT, rank+1, 1, MPI_COMM_WORLD);
-	}
-	else if (rank!=size-1) { 
-		MPI_Recv(b, 1, MPI_INT, rank-1, MPI_ANY_TAG, MPI_COMM_WORLD, &status); 
-		for (int i = ibeg; i < ((iend > MAX) ? MAX : iend); i++)
-		{
-			sum += a[i];
+
+		auto start = std::chrono::system_clock::now();
+
+		n = (int)max_size / size;
+		ibeg = rank * n;
+		iend = (rank + 1) * n;
+
+		if (rank == 0) {
+			sum = 0;
+
+			for (int i = ibeg; i < ((iend > max_size) ? max_size : iend); i++) {
+				sum += a[i];
+			}
+			MPI_Send(b, 1, MPI_INT, rank + 1, 1, MPI_COMM_WORLD);
 		}
-		printf("Process: %d, %d\n", rank, sum);
-		MPI_Send(b, 1, MPI_INT, rank+1, 1, MPI_COMM_WORLD);
-	}
-	else {
-		MPI_Recv(b, 1, MPI_INT, rank - 1, 1, MPI_COMM_WORLD, &status);
-		for (int i = ibeg; i < MAX; i++)
-		{
-			sum += a[i];
+		else if (rank != size - 1) {
+			MPI_Recv(b, 1, MPI_INT, rank - 1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			sum = 0;
+			for (int i = ibeg; i < ((iend > max_size) ? max_size : iend); i++) {
+				sum += a[i];
+			}
+			MPI_Send(b, 1, MPI_INT, rank + 1, 1, MPI_COMM_WORLD);
 		}
-		printf("Process: %d, %d\n", rank, sum);
+		else {
+			MPI_Recv(b, 1, MPI_INT, rank - 1, 1, MPI_COMM_WORLD, &status);
+			sum = 0;
+			for (int i = ibeg; i < max_size; i++) {
+				sum += a[i];
+			}
+		}
+
+		auto end = std::chrono::system_clock::now();
+		std::chrono::duration<double> elapsed_seconds = end - start;
+		total_elapsed_time += elapsed_seconds.count();
+
+		if (rank == size - 1) {
+			std::cout << "Total Elapsed Time for "<< max_size << " elements: " << elapsed_seconds.count() << " sec" << std::endl;
+		}
+
+		delete[] a;
 	}
-	
+
+	if (rank == size - 1) {
+		std::cout << "Total Elapsed Time for all sizes: " << total_elapsed_time << " sec" << std::endl;
+	}
+
 	MPI_Finalize();
-	auto end = std::chrono::system_clock::now();
 
-	std::chrono::duration<double> elapsed_seconds = end - start;
-	std::cout << "Elapsed Time: " << elapsed_seconds.count() << " sec" << std::endl;
-	
-	
+	delete[] processor_name;
 	return 0;
 }
